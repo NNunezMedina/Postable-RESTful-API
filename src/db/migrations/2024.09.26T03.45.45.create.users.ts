a0fed19975bf69650069cfa29751338909763da0
Nathaly <nathalynunezmedina@gmail.com>
2024-09-26 10:40:48 -0500

    "ADD migration logic for create table users"

--- src/db/migrations/2024.09.26T03.45.45.create.users.ts
+++ src/db/migrations/2024.09.26T03.45.45.create.users.ts
@@ -1,8 +1,19 @@
 import { Migration } from "../scripts/dbMigrate";
 
 export const up: Migration = async (params) => {
-  params.context.query(`RAISE EXCEPTION 'up migration not implemented'`);
+  return params.context.query(` CREATE TABLE users (
+      id SERIAL PRIMARY KEY,
+      username VARCHAR(255) UNIQUE NOT NULL,
+      password VARCHAR(255) NOT NULL,
+      email VARCHAR(255) UNIQUE,
+      first_name VARCHAR(255),
+      last_name VARCHAR(255),
+      role VARCHAR(50) NOT NULL DEFAULT 'user' CHECK (role IN ('user', 'admin')),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+      updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+      CHECK (email ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}$')
+    )`);
 };
 export const down: Migration = async (params) => {
-  params.context.query(`RAISE EXCEPTION 'down migration not implemented'`);
+  return params.context.query(`DROP TABLE users;`);
 };
