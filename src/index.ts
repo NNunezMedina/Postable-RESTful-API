b03fe3e71102f692ca88cca0bcb9d61d94e38587
Nathaly <nathalynunezmedina@gmail.com>
2024-09-26 19:03:45 -0500

    "ADD app.use for signup and error logic"

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import express from "express";
+import authRouter from "./db/routers/auth-router";
+import errorHandler from "./db/middleware/error";
 
 const app = express();
-const port = 5500;
+const port = 5502;
+
+app.use(express.json());
+app.use(authRouter);
+
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Escuchando al puerto ${port}`));
