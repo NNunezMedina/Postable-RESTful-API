da02c29dd54142a662b9a038fb8234b974cd6a90
Nathaly <nathalynunezmedina@gmail.com>
2024-09-26 22:22:13 -0500

    "ADD authenticate logic"

---
+++ src/db/middleware/authenticate.ts
@@ -0,0 +1,46 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { ApiError } from "./error";
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: string; 
+    }
+  }
+}
+
+const jwtSecret = process.env["JWT_SECRET"] || "your_jwt_secret_key";
+
+export function authenticateUser(
+  req: Request,
+  _res: Response,
+  next: NextFunction
+) {
+  const token = req.headers.authorization?.split(" ")[1];
+
+  console.log("Token recibido:", token); 
+
+  if (!token) {
+    return next(new ApiError("No autorizado", 401));
+  }
+
+  console.log("JWT Secret:", jwtSecret);
+
+  try {
+    const payload = jwt.verify(token, jwtSecret) as {
+      userId: number; 
+      userRole: string;
+      name: string;
+      iat: number;
+      exp: number;
+    };
+
+    req.user = payload.name; 
+    console.log("Usuario autenticado:", req.user); 
+    next();
+  } catch (error) {
+    console.error("Error de autenticación:", error);
+    return next(new ApiError("No autorizado", 401));
+  }
+}
